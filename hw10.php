ed0afce940827578f3f7ee6b6f1f0f7e6c89e377
m-dinic <milos.din1994@gmail.com>
2022-04-26 22:00:19 +0200

    formatirano

--- hw10.php
+++ hw10.php
@@ -1,66 +1,65 @@
-
 <html>
+
 <head>
     <title>hw10.php - MD</title>
 </head>
+
 <body>
     <?php
-        $var1= 200;
-        $var2= 80;
+    $var1 = 200;
+    $var2 = 80;
 
-        $a= 3;
-        $b= 7;
-        $c= 5;
+    $a = 3;
+    $b = 7;
+    $c = 5;
 
-//Prvi zadatak
+    //Prvi zadatak
 
-        echo "Zbir je: " . $var1 + $var2;
-        
-        echo "<br>";
+    echo "Zbir je: " . $var1 + $var2;
 
-        echo "Proizvod je: " . $var1 * $var2; 
-        
-        echo "<br>";
+    echo "<br>";
 
-        echo "Razlika je: " . $var1 - $var2; 
-        
-        echo "<br>";
+    echo "Proizvod je: " . $var1 * $var2;
 
-        echo "Kolicnik je: " . $var1 / $var2; 
-        
-        echo "<br>";
-        echo "<br>";
+    echo "<br>";
 
-//Drugi zadatak
-        $rand= rand(0,6);
-        if ($rand==0){
-            echo "Danas je ponedeljak";
-        } elseif ($rand==1) {
-            echo "Danas je utorak";
-        } elseif ($rand==2) {
-            echo "Danas je sreda";
-        } elseif ($rand==3) {
-            echo "Danas je cetvrtak";
-        } elseif ($rand==4) {
-            echo "Danas je petak";
-        } elseif ($rand==5) {
-            echo "Danas je subota";
-        } else {
-            echo "Danas je nedelja";
-        }
+    echo "Razlika je: " . $var1 - $var2;
 
+    echo "<br>";
 
-        echo "<br>";
-        echo "<br>";
-//Treci zadatak
+    echo "Kolicnik je: " . $var1 / $var2;
 
-        echo "Zbir brojeva $a, $b i $c je: ". $a+$b+$c;
-        
+    echo "<br>";
+    echo "<br>";
+    //Drugi zadatak
+    $rand = rand(0, 6);
+    if ($rand == 0) {
+        echo "Danas je ponedeljak";
+    } elseif ($rand == 1) {
+        echo "Danas je utorak";
+    } elseif ($rand == 2) {
+        echo "Danas je sreda";
+    } elseif ($rand == 3) {
+        echo "Danas je cetvrtak";
+    } elseif ($rand == 4) {
+        echo "Danas je petak";
+    } elseif ($rand == 5) {
+        echo "Danas je subota";
+    } else {
+        echo "Danas je nedelja";
+    }
+    echo "<br>";
+    echo "<br>";
+    //Treci zadatak
+
+    echo "Zbir brojeva $a, $b i $c je: " . $a + $b + $c;
+
     ?>
 
 
 
 
-    
+
 </body>
+
 </html>
